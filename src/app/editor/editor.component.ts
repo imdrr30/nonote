c27aace8d27d8397da50dbb3a09f60ac9b3c74ff
Revan <revan@Revans-MacBook-Air.local>
2025-01-01 10:47:00 +0530

    fixed bg color

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -106,7 +106,6 @@ export class EditorComponent implements AfterViewInit {
 					'strikethrough',
 					'subscript',
 					'superscript',
-					'code',
 					'codeBlock',
 					'removeFormat',
 					'|',
